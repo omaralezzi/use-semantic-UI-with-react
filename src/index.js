08ddc2155c7dbc3c8e2cab380a51c146367e7af4
omaralezzi <81753019+omaralezzi@users.noreply.github.com>
2021-11-06 16:50:18 +0100

    Usng Semantic UI and Faker js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 // import App from "./App";
 const App = () => {
@@ -7,7 +8,7 @@ const App = () => {
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" />
+          <img alt="avatar" src={faker.image.avatar()} />
         </a>
         <div className="content">
           <a href="/" className="author">
@@ -22,7 +23,8 @@ const App = () => {
     </div>
   );
 };
-
+const sss = faker.time.recent();
+console.log(sss);
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>

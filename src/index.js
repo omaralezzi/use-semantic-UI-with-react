a180dabc67eeb47de5bbd07e867bb800104b20c3
omaralezzi <81753019+omaralezzi@users.noreply.github.com>
2021-11-06 20:19:42 +0100

    reuse Props inside cards

--- src/index.js
+++ src/index.js
@@ -2,36 +2,55 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import CommmentDetail from "./CommentDetail";
 import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
 // import App from "./App";
 const App = () => {
+  // another way to provide props with value from Faker
   let imageFromFakerJs = faker.image.avatar;
 
   return (
     <div className="ui container comments">
-      <CommmentDetail
-        author="Sam"
-        timeAgo="Today at 6:00 PM"
-        commentText="hello"
-        src={imageFromFakerJs()}
+      <ApprovalCard>
+        <CommmentDetail
+          author="Sam"
+          timeAgo="Today at 6:00 PM"
+          commentText="hello"
+          // src={imageFromFakerJs()}
 
-        // src={faker.image.avatar()}
-      />
-      <CommmentDetail
-        author="Toni"
-        timeAgo="yesterday at 2:00 PM"
-        commentText="good morning"
-        src={imageFromFakerJs()}
+          src={faker.image.avatar()}
+        />
+      </ApprovalCard>
 
-        // src={faker.image.avatar()}
-      />
+      {/* <ApprovalCard /> */}
+      <ApprovalCard>
+        <CommmentDetail
+          author="Toni"
+          timeAgo="yesterday at 2:00 PM"
+          commentText="good morning"
+          // src={imageFromFakerJs()}
+
+          src={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommmentDetail
+          author="Cher"
+          timeAgo="Today at 8:00 PM"
+          commentText="hi everyone"
+          // src={imageFromFakerJs()}
+
+          src={faker.image.avatar()}
+        />
+      </ApprovalCard>
       <CommmentDetail
         author="Cher"
-        timeAgo="Today at 8:00 PM"
-        commentText="hi everyone"
-        src={imageFromFakerJs()}
+        timeAgo="Today at 9:10 PM"
+        commentText="Moin Moin"
+        // src={imageFromFakerJs()}
 
-        // src={faker.image.avatar()}
+        src={faker.image.avatar()}
       />
     </div>
   );
